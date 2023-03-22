3e35690050b9d4591822020c93a99a92060e06c9
luciPark <lucipark05@gmail.com>
2023-03-22 10:27:45 +0900

    WallDataAdded

--- Client/LTiles.cpp
+++ Client/LTiles.cpp
@@ -1,7 +1,9 @@
 #include "LTiles.h"
 namespace cl
 {
-	const int TileInfo::tileUnit = 27;
+	const int TileInfo::tileUnit = 24;
+	const Vector2 TileInfo::floorSpriteSize = Vector2(27.0f, 27.0f);
+	const Vector2 TileInfo::wallSpriteSize = Vector2(24.0f, 48.0f);
 	const float TileInfo::tileScale = 2.0f;
 	const Vector2 TileInfo::floorIndex[eFloorTypes::Size] = {
 		Vector2(0, 0), // DarkFloor1
@@ -22,14 +24,62 @@ namespace cl
 		Vector2(2, 8),// ClosedStairs
 		Vector2(2, 6)// OpenedStairs
 	};
+	const Vector2 TileInfo::wallIndex[eWallTypes::Size] = {
+		Vector2(0, 0), //Wall1,
+		Vector2(0, 1), //Wall2,
+		Vector2(0, 2), //Wall3,
+		Vector2(0, 3), //Wall4,
+		Vector2(0, 4), //Wall5,
+		Vector2(0, 5), //Wall6,
+		Vector2(0, 6), //Wall7,
+		Vector2(0, 7), //Wall8,
+		Vector2(0, 8), //Wall9,
+		Vector2(0, 9), //Wall10,
+		Vector2(0, 10), //Wall11,
+		Vector2(0, 11), //Wall12,
+		Vector2(0, 12), //Wall13,
+		Vector2(0, 13), //Wall14,
+		Vector2(0, 14), //Wall15,
+		Vector2(0, 15),	//Wall16,
+		Vector2(0, 29), //HarderWall,
+		Vector2(0, 31), //StoneWall1,
+		Vector2(0, 32), //StoneWall2,
+		Vector2(0, 34), //BossWall1,
+		Vector2(0, 35), //BossWall2,
+		Vector2(0, 36), //BossWall3,
+		Vector2(0, 37), //BossWall4,
+		Vector2(0, 38), //BossWall5,
+		Vector2(6, 0), //GoldWall,
+		Vector2(6, 4), //Border1,
+		Vector2(6, 5), //Border2,
+		Vector2(6, 6), //Border3,
+		Vector2(6, 7), //Border4,
+		Vector2(6, 8), //Border5,
+		Vector2(6, 9), //Border6,
+		Vector2(6, 10), //Border7,
+		Vector2(6, 11), //Border8,
+		Vector2(6, 12), // DoorHorizonal,
+		Vector2(6, 13), // DoorVertical
+	};
 
 	Sprite TileInfo::GetFloorTile(eFloorTypes tileType)
 	{
 		Sprite sprite;
 		Vector2 index = floorIndex[(int)tileType];
-		sprite.leftTop = index * tileUnit;
-		sprite.size.x = tileUnit;
-		sprite.size.y = tileUnit;
+		sprite.leftTop.x = index.x * floorSpriteSize.x;
+		sprite.leftTop.y = index.y * floorSpriteSize.y;
+		sprite.size = floorSpriteSize;
+		sprite.offset -= sprite.size * 0.5;
+		return sprite;
+	}
+
+	Sprite TileInfo::GetWallTile(eWallTypes tileType)
+	{
+		Sprite sprite;
+		Vector2 index = wallIndex[(int)tileType];
+		sprite.leftTop.x = index.x * wallSpriteSize.x;
+		sprite.leftTop.y = index.y * wallSpriteSize.y;
+		sprite.size = wallSpriteSize;
 		sprite.offset -= sprite.size * 0.5;
 		return sprite;
 	}
--- Client/LTiles.h
+++ Client/LTiles.h
@@ -25,13 +25,57 @@ namespace cl
 		Size
 	};
 
+	enum eWallTypes 
+	{
+		Wall1,
+		Wall2,
+		Wall3,
+		Wall4,
+		Wall5,
+		Wall6,
+		Wall7,
+		Wall8,
+		Wall9,
+		Wall10,
+		Wall11,
+		Wall12,
+		Wall13,
+		Wall14,
+		Wall15,
+		Wall16,
+		HarderWall,
+		StoneWall1,
+		StoneWall2,
+		BossWall1,
+		BossWall2,
+		BossWall3,
+		BossWall4,
+		BossWall5,
+		GoldWall,
+		Border1,
+		Border2,
+		Border3,
+		Border4,
+		Border5,
+		Border6,
+		Border7,
+		Border8,
+		DoorHorizontal,
+		DoorVertical,
+		Size
+	};
+
 	struct TileInfo
 	{
 		static const int tileUnit;
+		static const Vector2 floorSpriteSize;
+		static const Vector2 wallSpriteSize;
 		static const float tileScale;
 		static const Vector2 floorIndex[];
+		static const Vector2 wallIndex[];
 
 		static Sprite GetFloorTile(eFloorTypes tileType);
+		static Sprite GetWallTile(eWallTypes tileType);
 	};
 }
 
